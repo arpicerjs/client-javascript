f42264e16442f279269d13e13f71221c0afc9b26
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-11-17 13:26:24 +0700

    create call method

--- hamjs-rpc.js
+++ hamjs-rpc.js
@@ -6,5 +6,27 @@ const rpc = ((w, d) => {
     return self;
   };
 
+  self.call = function call(procedure, ...params) {
+    return new Promise(function(resolve, reject) {
+      const body = JSON.stringify({
+        call: procedure,
+        parameters: params,
+      });
+
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function onreadystatechange(e) {
+        if (e.target.readyState === 4) {
+          if (e.target.status == 200) {
+            resolve(e.target.responseText);
+          }
+          reject(e);
+        }
+      };
+
+      xhr.open('POST', self.host);
+      xhr.send(body);
+    });
+  };
+
   return self;
 })(window, document);
